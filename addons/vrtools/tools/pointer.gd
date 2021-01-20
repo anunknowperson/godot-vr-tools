10d4fd9038976cdfa83d2fb2fde944c5d9a3f9c9
anunknowperson <kiselev.sereja@gmail.com>
2021-01-20 19:28:53 +0700

    Added UI

--- addons/vrtools/tools/pointer.gd
+++ addons/vrtools/tools/pointer.gd
@@ -1,9 +1,56 @@
 tool
 extends Spatial
 
+export (int) var max_raycast_distance = 100
+export (String) var ui_group = "ui_collider"
+export (int) var button_index = 15
+
+var is_pressed :bool = false
+var space_state :PhysicsDirectSpaceState
+var point :Vector3
+var body :StaticBody
 
 func _enter_tree():
-	pass
+	if not Engine.editor_hint:
+		space_state = get_parent().get_world().direct_space_state
+
+func _physics_process(delta):
+	if not Engine.editor_hint:
+		var result = space_state.intersect_ray(global_transform.origin, global_transform.origin + global_transform.basis.z * -1 * max_raycast_distance)
+
+		if not result.empty() and result.collider.is_in_group(ui_group):
+			$LineRenderer.points = [global_transform.origin, result.position]
+			$LineRenderer.show()
+			
+			body = result.collider
+			point = result.position
+			
+			if is_pressed:
+				body.get_parent().get_parent().ui_raycast_hit_event(point, false, false)
+		else:
+			$LineRenderer.hide()
+			
+			body = null
+
+func _input(event):
+	if event.device + 1 != get_parent().controller_id:
+		return
+	
+	if not Engine.editor_hint:
+		if event is InputEventJoypadButton:
+			if event.button_index == button_index:
+				if event.pressed:
+					if not is_pressed:
+						if body != null:
+							body.get_parent().get_parent().ui_raycast_hit_event(point, true, false)
+							
+						is_pressed = true
+				else:
+					if is_pressed:
+						if body != null:
+								body.get_parent().get_parent().ui_raycast_hit_event(point, false, true)
+						
+						is_pressed = false
 
 func _get_configuration_warning() -> String:
 	if not get_parent().get_class() == "ARVRController":
--- addons/vrtools/tools/ui.gd
+++ addons/vrtools/tools/ui.gd
@@ -6,9 +6,41 @@ export (NodePath) var mesh
 export (NodePath) var control
 
 var controln : Control
+var viewportn :Viewport
+
+var prev_pos = null
+var last_click_pos = null
+var last_pos2d = null
+
+func ui_raycast_hit_event(position, click, release):
+	var local_position = to_local(position);
+	var pos2d = Vector2(local_position.x + get_node(mesh).scale.x / 2, get_node(mesh).scale.z-local_position.y - get_node(mesh).scale.z / 2) * 100
+
+	print(pos2d)
+
+	if (click || release):
+		var e = InputEventMouseButton.new()
+		e.pressed = click
+		e.button_index = BUTTON_LEFT
+		e.position = pos2d
+		e.global_position = pos2d
+
+		viewportn.input(e)
+		
+	elif (last_pos2d != null && last_pos2d != pos2d):
+		var e = InputEventMouseMotion.new()
+		e.relative = pos2d - last_pos2d
+		e.speed = (pos2d - last_pos2d) / 16.0
+		e.global_position = pos2d
+		e.position = pos2d
+		
+		viewportn.input(e)
+	last_pos2d = pos2d
 
 func _enter_tree():
 	if not Engine.editor_hint:
+		viewportn = get_node(viewport);
+		
 		controln = get_node(control)
 		
 		if get_node(viewport).get_child_count() > 0:
@@ -19,7 +51,7 @@ func _enter_tree():
 		
 		get_node(viewport).size = Vector2(controln.margin_right, controln.margin_bottom)
 		
-		get_node(mesh).scale = Vector3(controln.margin_right / 100, controln.margin_bottom / 100, 1)
+		get_node(mesh).scale = Vector3(controln.margin_right / 100, 1, controln.margin_bottom / 100)
 		
 		update_image()
 
